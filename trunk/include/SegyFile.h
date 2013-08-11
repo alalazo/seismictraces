623da615d656be915cde86c20cd5bb12eb985547
m.culpo <m.culpo@36a8e6c5-89c3-4a22-8c42-85c958d63b5e>
2013-08-11 14:40:38 +0000

    Added documentation to SEG Y file class. Added stream operator<< to trace header.

--- trunk/include/SegyFile.h
+++ trunk/include/SegyFile.h
@@ -12,29 +12,183 @@
 #include<impl/SegyFile-constants.h>
 
 #include<string>
+#include<vector>
 #include<fstream>
 #include<iostream>
 #include<ios>
+#include<utility>
+
+#include<stdint.h>
 
 namespace seismic {
     
+    /**
+     * @brief Models a file conforming to SEG Y rev 1 format
+     * 
+     * According to SEGY rev 1 standard (May 2002):
+     * 
+     * >
+     * > ## SEG Y File Structure
+     * > 
+     * > The original SEG Y standard was produced at a time when 9-track tape
+     * > was the normal storage medium for seismic data, with 800 bpi and 1600
+     * > bpi in common usage and 6250 bpi on the horizon. The revised format is
+     * > intended to be independent of the actual medium on which it is 
+     * >recorded.
+     * >
+     * > The stipulation in the 1975 standard that “no more than one line of 
+     * > seismic data is permitted on any one reel” became impractical long ago.
+     * > The term “SEG Y file” shall be used in this revision in place of the 
+     * > term “seismic reel” used in the original standard. For this standard, 
+     * > the terms file and data set are synonymous. Both terms are a collection
+     * > of logically related data traces or ensembles of traces and the 
+     * > associated ancillary data.
+     * >
+     * > ...
+     * >
+     * > ### File Structure
+     * > The first 3600-bytes of the file are the Textual File Header and the 
+     * > Binary File Header written as a concatenation of a 3200-byte record 
+     * > and a 400-byte record. This is optionally followed by Extended Textual 
+     * > File Header(s), which consists of zero or more 3200-byte Extended 
+     * > Textual File Header records. The remainder of the SEG Y file contains 
+     * > a variable number of Data Trace records that are each proceeded by a 
+     * > 240-byte Trace Header. The Extended Textual File Header is the only 
+     * > structural change introduced in this revision and while not strictly 
+     * > downward compatible with the 1975 SEG Y format, it has been carefully 
+     * > designed to have minimal impact on existing SEG Y reader software. It 
+     * > should be simple for existing software to be modified to detect the 
+     * > presence of the new header and either process or ignore the Extended 
+     * > Textual File Header.
+     * >
+     * > ### Number Formats
+     * > In the 1975 SEG Y standard, all binary values are defined as using 
+     * > “big-endian” byte ordering. This conformed to the IBM tape standard and
+     * > means that, within the bytes that make up a number, the most 
+     * > significant byte (containing the sign bit) is written closest to the 
+     * > beginning of the file and the least significant byte is written closest
+     * > to the end of the file. This byte ordering convention is maintained in 
+     * > this revision of the SEG Y format and it should be adhered to for all 
+     * > conforming versions of SEG Y. This is independent of the medium to 
+     * > which a particular SEG Y file is written (i.e. the byte ordering is no
+     * > different if the file is written to tape on a mainframe or to disk on
+     * > a PC).
+     * >
+     * > All values in the Binary File Header and the Trace Header are two's 
+     * > complement integers, either two bytes or four bytes long. There are no 
+     * > floating-point values defined in the headers.
+     * > 
+     * > Trace Data sample values are either two's complement integers or 
+     * > floating-point. This revision adds data sample formats of 8-bit integer
+     * > and 32-bit IEEE floating-point. Both IBM floating-point (as defined in 
+     * > the original standard) and IEEE floating-point values are written in 
+     * > big-endian byte order (i.e. with the sign/exponent byte written 
+     * > closest to the beginning of the file).
+     * >
+     * > ### Varying Trace Lengths
+     * > The SEG Y standard specifies fields for sampling interval and number 
+     * > of samples at two separate locations in the file. The Binary File 
+     * > Header contains values that apply to the whole file and the Trace 
+     * > Headers contain values that apply to the associated trace. The original
+     * > standard is unclear about how these are to be used together. One view 
+     * > is that variable length traces are supported in SEG Y, with the number 
+     * > of samples in the trace header allowed to vary from trace to trace and 
+     * > to vary from the value in the Binary File Header. An alternate view is 
+     * > that all traces in a SEG Y file will be the same length and the value 
+     * > for the number of samples will be the same in the Binary File Header 
+     * > and all Trace Headers. In the second case, the data traces are padded 
+     * > or truncated as necessary.
+     * >
+     * > In SEGY rev 1, varying trace lengths in a file are explicitly allowed.
+     * > The values for sampling interval and number of samples in the Binary 
+     * > File Header should be for the primary set of seismic data traces in the
+     * > file. This approach allows the Binary File Header to be read and say, 
+     * > for instance, “this is six seconds data sampled at a two-millisecond 
+     * > sampling interval”. The value for the number of samples in each 
+     * > individual Trace Header may vary from the value in the Binary File 
+     * > Header and reflect the actual number of samples in a trace. The number 
+     * > of bytes in each trace record must be consistent with the number of 
+     * > samples in the Trace Header.  This is particularly important for SEG Y 
+     * > data written to disk files.
+     * >
+     * > Allowing variable length traces dictates sequential access and 
+     * > precludes random access in a disk file, since the locations of traces 
+     * > after the first are not known. To enable the option of random access, 
+     * > a new field in the Binary File Header has been defined as a fixed 
+     * > length trace flag. If this flag is set, all traces in the file must 
+     * > have the same length. This will typically be the case for poststack 
+     * > data.
+     * >
+     * > Making the value for number of samples in the Binary File Header the 
+     * > maximum trace length in the file, rather than the length of the primary
+     * > set of data traces was seriously considered.  However, it should be 
+     * > noted that the maximum trace length is not necessarily known at the 
+     * > time the Binary File Header is written, particularly in a transcription
+     * > environment. This is the same reason why there are no fields in the 
+     * > Binary File Header such as “first and last record number”. The fixed 
+     * > length trace flag goes some way to ameliorating the problems induced by
+     * > having variable length traces. If the fixed record length flag is set, 
+     * > the maximum trace length in the file is known (i.e. all traces are 
+     * > the same length).
+     * >
+     * > ### Coordinates 
+     * > Knowing the source and trace locations is a primary requirement for 
+     * > processing seismic data, and knowing the location of the processed data
+     * > with respect to other data is essential for interpretation. 
+     * > Traditionally seismic coordinates have been supplied as geographic 
+     * > coordinates and/or grid coordinates. SEG Y accommodates either form. 
+     * > However locations are ambiguous without clear coordinate reference 
+     * > system (CRS) definition. SEG Y rev 1 significantly expands the ability 
+     * > to define the CRS used for the coordinates contained within the Binary 
+     * > Header, the Extended Textual Headers and the Trace Headers. A single 
+     * > CRS must be used for all coordinates within an individual SEG Y data 
+     * > set.  Additionally the coordinate units must be the same for all 
+     * > coordinates.
+     * 
+     * The following is a small example of the basic use of the class:
+     * @include example05.cpp
+     * 
+     */
     class SegyFile {
     public:
+
+        /// Trace header plus corresponding trace data
+        typedef std::pair< TraceHeader, SeismicTrace > trace_type;
         
+        /**
+         * @brief Constructor of a SegyFile
+         * 
+         * @param[in] filename name of the SEG Y file to be read/written
+         * @param[in,out] tfh Textual file header
+         * @param[in,out] bfh Binary file header
+         * @param[in] mode open mode of the file
+         * 
+         */
         SegyFile(
                 const char * filename, TextualFileHeader& tfh, BinaryFileHeader& bfh,
                 std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out
                 );
         
         
-        void read  (SeismicTrace& trace, int n) const;
-        void append(SeismicTrace& trace);
+        trace_type read(int n) const;
+        void append(const trace_type& trace);
         
-    private:        
-        std::fstream       fstream_;        
-        
+    private:
+        //////////
+        // File related information
+        //////////
+        std::fstream       fstream_;                
         TextualFileHeader& tfh_;
         BinaryFileHeader&  bfh_;
+        //////////
+        // Bookkeeping variables to permit random access to traces
+        //////////
+        size_t              sizeOfDataSample_;
+        std::vector<size_t> traceSeekStrides_;
+        //////////
+        // Extended textual file headers
+        //////////
+        int16_t nextendedTextualFileHeader_;        
     };
         
 }
--- trunk/src/SegyFile.cpp
+++ trunk/src/SegyFile.cpp
@@ -3,26 +3,53 @@
 #include<impl/utilities-inl.h>
 
 #include<algorithm>
+#include<stdexcept>
+#include<sstream>
 
 using namespace std;
 
 namespace {
-    
+
+    /**
+     * @brief Functor that given a field of a binary header file, swaps its byte order
+     */
     class BfhSwapByteOrder {
     public:
-        BfhSwapByteOrder(seismic::BinaryFileHeader& bfh) : bfh_(bfh) {            
+
+        BfhSwapByteOrder(seismic::BinaryFileHeader& bfh) : bfh_(bfh) {
         }
-        
+
         void operator()(seismic::BinaryFileHeader::Int16Fields idx) {
             seismic::swapByteOrder(bfh_[idx]);
         }
-        
+
         void operator()(seismic::BinaryFileHeader::Int32Fields idx) {
             seismic::swapByteOrder(bfh_[idx]);
         }
-        
+
     private:
         seismic::BinaryFileHeader& bfh_;
+    };
+
+    /**
+     * @brief Functor that given a field of a trace header file, swaps its byte order
+     */
+    class ThSwapByteOrder {
+    public:
+
+        ThSwapByteOrder(seismic::TraceHeader& th) : th_(th) {
+        }
+
+        void operator()(seismic::TraceHeader::Int16Fields idx) {
+            seismic::swapByteOrder(th_[idx]);
+        }
+
+        void operator()(seismic::TraceHeader::Int32Fields idx) {
+            seismic::swapByteOrder(th_[idx]);
+        }
+
+    private:
+        seismic::TraceHeader& th_;
     };
 }
 
@@ -47,24 +74,123 @@ namespace seismic {
             // Read Binary file header  (400 bytes)
             fstream_.read(bfhBuffer, BinaryFileHeader::buffer_size);
 #ifdef LITTLE_ENDIAN
-            // If the system is little endian, bytes must be swapped
-            BfhSwapByteOrder swapVisitor(bfh_);
-            for_each( BinaryFileHeader::Int32List.begin(),BinaryFileHeader::Int32List.end(),swapVisitor );            
-            for_each( BinaryFileHeader::Int16List.begin(),BinaryFileHeader::Int16List.end(),swapVisitor );            
-//          for (int ii = 0; ii < 400; ii++) {
-//              cout << std::dec << std::noshowbase << ii << ' ';
-//              cout << std::hex << std::showbase << (short) bfhBuffer[ii] << endl;
-//          }
+            {
+                // If the system is little endian, bytes must be swapped
+                BfhSwapByteOrder swapVisitor(bfh_);
+                for_each(BinaryFileHeader::Int32List.begin(), BinaryFileHeader::Int32List.end(), swapVisitor);
+                for_each(BinaryFileHeader::Int16List.begin(), BinaryFileHeader::Int16List.end(), swapVisitor);
+            }
+#endif            
+            // Compute the correct size of a single data sample
+            switch ( bfh_[BinaryFileHeader::formatCode] ) {
+                case ( constants::SegyFileFormatCode::IBMfloat32 ):
+                case ( constants::SegyFileFormatCode::Int32      ):
+                case ( constants::SegyFileFormatCode::Fixed32    ):
+                case ( constants::SegyFileFormatCode::IEEEfloat32):
+                    sizeOfDataSample_ = 4;
+                    break;
+                case ( constants::SegyFileFormatCode::Int16      ):
+                    sizeOfDataSample_ = 2;
+                    break;
+                case ( constants::SegyFileFormatCode::Int8       ):
+                    sizeOfDataSample_ = 1;
+                    break;
+                default:
+                    throw runtime_error("FATAL ERROR: invalid \"Data sample format code\"\n");
+                    break;
+            }
+
+            //////////
+            // Check the presence of extended textual file header
+            //////////
+            nextendedTextualFileHeader_ = bfh_[BinaryFileHeader::nextendedTextualFileHeader];
+            if ( nextendedTextualFileHeader_ < -1 ) {
+                stringstream estream;
+                estream << "FATAL ERROR: " << filename << " is a non-conforming SEG Y file" << endl << endl;
+                estream << "In binary file header the number of extended textual file header is < -1" << endl;
+                throw runtime_error( estream.str() );
+            }
+            // Read Extended textual file headers
+            /// TODO: to be implemented
+
+            //////////
+            // Construct the vector of strides to permit random access to traces
+            //////////
+
+            size_t segyFileSize = fstream_.seekg(0, ios::end).tellg();
+            // FIXME: change as soon as Extended textual file header is implemented
+            size_t currentStride = TextualFileHeader::line_length * TextualFileHeader::nlines + BinaryFileHeader::buffer_size + nextendedTextualFileHeader_ * 3200;
+            traceSeekStrides_.push_back( currentStride );
+            if ( bfh_[BinaryFileHeader::fixedLengthTraceFlag] == 1 ) {
+                //
+                // If the traces have all fixed length the strides can be
+                // computed without inspecting the file sequentially
+                //
+
+                while ( true ) {
+                    currentStride += TraceHeader::buffer_size + sizeOfDataSample_ * bfh_[BinaryFileHeader::nsamplesDataTrace];
+                    if ( segyFileSize == currentStride) {
+                        break;
+                    } else if ( segyFileSize < currentStride) {
+                        stringstream estream;
+                        estream << "FATAL ERROR: " << filename << " is truncated after trace number " << traceSeekStrides_.size() << endl << endl;
+                        estream << "The total size of the file is " << segyFileSize << " while it should be " << currentStride;
+                        estream << " to contain " << (traceSeekStrides_.size() + 1) << "traces" << endl;
+                        throw runtime_error( estream.str() );
+                    }
+
+                    traceSeekStrides_.push_back( currentStride );
+
+                }
+            } else if ( bfh_[BinaryFileHeader::fixedLengthTraceFlag] == 0 ) {
+                //
+                // If the traces does not have fixed length, the file must be 
+                // inspected sequentially at least once
+                //
+                TraceHeader th;
+
+                while ( true ) {
+                    // Move to the start of the next trace header and read it
+                    fstream_.seekg( currentStride ).read( th.get(), TraceHeader::buffer_size );
+#ifdef LITTLE_ENDIAN
+                    {
+                        // If the system is little endian, bytes must be swapped
+                        ThSwapByteOrder swapVisitor(th);
+                        for_each(TraceHeader::Int32List.begin(), TraceHeader::Int32List.end(), swapVisitor);
+                        for_each(TraceHeader::Int16List.begin(), TraceHeader::Int16List.end(), swapVisitor);
+                    }
+#endif    
+                    currentStride += TraceHeader::buffer_size + sizeOfDataSample_ * th[TraceHeader::nsamplesTrace];
+                    if ( segyFileSize == currentStride) {
+                        break;
+                    } else if ( segyFileSize < currentStride) {
+                        stringstream estream;
+                        estream << "FATAL ERROR: " << filename << " is truncated after trace number " << traceSeekStrides_.size() << endl << endl;
+                        estream << "The total size of the file is " << segyFileSize << " while it should be " << currentStride;
+                        estream << " to contain " << (traceSeekStrides_.size() + 1) << "traces" << endl;
+                        throw runtime_error( estream.str() );
+                    }
+
+                    traceSeekStrides_.push_back( currentStride );
+                }
+
+            } else {
+                stringstream estream;
+                estream << "FATAL ERROR: " << filename << " is a non-conforming SEG Y file" << endl << endl;
+                estream << "In binary file header the 'fixed length trace flag' (bytes 3503-3504)";
+                estream << " has the invalid value of " << bfh_[BinaryFileHeader::fixedLengthTraceFlag];
+                estream << " (valid values are 0/1) " << endl;
+                throw runtime_error( estream.str() );
+            }
             
-#endif
         } else {
             ////////////////////
             // If file was opened for writing tfh and bfh 
             // must be written to a newly created file
             ////////////////////
-            
+
             // TO BE IMPLEMENTED
         }
     }
-    
+
 }
