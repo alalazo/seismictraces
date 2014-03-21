ef5528b0f6425e97f1fa311bdfcd5cf5fae26660
Massimiliano Culpo <alalazo@users.noreply.github.com>
2014-03-21 18:59:24 +0000

    Added license statement in files Added copy of GPL v3 and LGPL v3 to the project

--- examples/example_macro.h
+++ examples/example_macro.h
@@ -1,7 +1,27 @@
+/*
+ *  SeismicTraces : another C++ library that reads files in SEG-Y format
+ * 
+ *  Copyright (C) 2014  Massimiliano Culpo
+ * 
+ *  This file is part of SeismicTraces.
+ *
+ *  SeismicTraces is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SeismicTraces is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SeismicTraces.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef EXAMPLE_MACRO_CMAKE_H
 #define	EXAMPLE_MACRO_CMAKE_H
 
-#define DATA_FOLDER "/home/mculpo/eni/seismicTraces/data"
+#define DATA_FOLDER "/home/mculpo/sourceforge/seismic-traces/sources/data"
 
 #endif	/* EXAMPLE_MACRO_CMAKE_H */
 
