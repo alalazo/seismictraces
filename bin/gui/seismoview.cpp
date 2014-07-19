5ae9735a02278a74fb7e6a166fb054971a4ccf25
Massimiliano Culpo <alalazo@users.noreply.github.com>
2014-07-19 12:53:16 +0000

    Added stub main for gui application

---
+++ bin/gui/seismoview.cpp
@@ -0,0 +1,41 @@
+/*
+ *  SeismicTraces : another C++ library that reads files in SEG-Y format
+ * 
+ *  Copyright (C) 2014  Massimiliano Culpo
+ * 
+ *  This file is part of SeismicTraces.
+ *
+ *  SeismicTraces is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SeismicTraces is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SeismicTraces.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file seismoview.cpp
+ * @brief GUI application built on top of the SeismicTraces library
+ */
+
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+    QLabel *label = new QLabel("Hello Qt!"); 
+    label->show();
+    
+    return app.exec();
+}
