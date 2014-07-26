9d1c654ebd5d1787c29f5d85420f739ccc4fb7d4
alalazo <alalazo@36a8e6c5-89c3-4a22-8c42-85c958d63b5e>
2014-07-26 16:16:44 +0000

    Added factory for colormap Changed style to Fusion dark Fixed issue with visualization of TFH

--- trunk/bin/gui/ui/segycolormap.cpp
+++ trunk/bin/gui/ui/segycolormap.cpp
@@ -9,10 +9,12 @@
 #include <qwt_raster_data.h>
 
 #include <algorithm>
+#include <limits>
 #include <vector>
 #include <cstdint>
 
 namespace {
+template<class T>
 class SegyTraceData: public QwtRasterData
 {
 public:
@@ -21,10 +23,10 @@ public:
         using namespace seismic;
 
         size_t max_nsamples = 0;
-        float min_value = INT16_MAX;
-        float max_value = INT16_MIN;
+        T min_value = std::numeric_limits<T>::max();
+        T max_value = std::numeric_limits<T>::lowest();
         for( size_t ii = 0; ii < file->ntraces(); ++ii) {
-            m_traces.push_back( std::move(file->readTraceAs<float>(ii) ) );
+            m_traces.push_back( std::move(file->readTraceAs<T>(ii) ) );
             max_nsamples = std::max(max_nsamples,m_traces[ii].size());
             auto minmax_value = std::minmax_element(m_traces[ii].begin(),m_traces[ii].end());
             min_value = std::min(min_value,*minmax_value.first);
@@ -40,17 +42,50 @@ public:
         return m_traces[x][y];
     }
 private:
-    std::vector< seismic::Trace<float> > m_traces;
+    std::vector< seismic::Trace<T> > m_traces;
 };
-}
 
+/**
+ * @brief Poor man's factory to return the correct raster data type
+ * @param[in] file handle to the SEG-Y file
+ * @return raster data
+ */
+QwtRasterData * createSegyTraceData(std::shared_ptr<seismic::SegyFile> file) {
+    using namespace seismic;
+
+    auto& bfh = file->getBinaryFileHeader();
+    auto format = bfh[rev0::bfh::formatCode];
+    switch( format ) {
+    case (constants::SegyFileFormatCode::IBMfloat32):
+    case (constants::SegyFileFormatCode::IEEEfloat32):
+    {
+        return new SegyTraceData<float>(file);
+    }
+    case (constants::SegyFileFormatCode::Int32):
+    {
+        return new SegyTraceData<int32_t>(file);
+    }
+    case (constants::SegyFileFormatCode::Int16):
+    {
+        return new SegyTraceData<int16_t>(file);
+    }
+    case (constants::SegyFileFormatCode::Int8):
+    {
+        return new SegyTraceData<int8_t>(file);
+    }
+    default:
+        break;
+    }
+    return nullptr;
+}
+}
 SegyColormap::SegyColormap(std::shared_ptr<seismic::SegyFile> file, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::SegyColormap)
 {
     ui->setupUi(this);
     auto spectrogram = new QwtPlotSpectrogram();
-    spectrogram->setData( new SegyTraceData(file) );
+    spectrogram->setData( createSegyTraceData(file) );
     spectrogram->attach(ui->colormap);
     ui->colormap->setTitle("Trace amplitude ");
     ui->colormap->setAxisTitle( QwtPlot::yRight , "sample #");
